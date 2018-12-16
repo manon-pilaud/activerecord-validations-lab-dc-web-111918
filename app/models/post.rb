63126905fb07ec6a71952b19ce9e2f01945edebb
Manon Pilaud <mandyypilaud@gmail.com>
2018-12-16 12:43:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum:250}
+  validates :summary, length: {maximum:250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+   def clickbait?
+     if self.title
+       unless self.title.match(/Secret|Top|Guess|Won't Believe/)
+         errors.add(:title, "must be clickbait")
+       end
+     end
+
+  end
 end
